495553a075fc0d007c6c7925b353798bf95ae6bc
Alistair Riddoch <alriddoch@googlemail.com>
2014-12-15 22:39:06 +0000

    Update to include CXXFLAGS

--- 51_SDL_and_modern_opengl/build.sh
+++ 51_SDL_and_modern_opengl/build.sh
@@ -1,1 +1,1 @@
-export LDLIBS="$(sdl2-config --libs) -lGL -lGLEW" && make 51_SDL_and_modern_opengl && ./51_SDL_and_modern_opengl
+export CXXFLAGS=$(sdl2-config --cflags) && export LDLIBS="$(sdl2-config --libs) -lGL -lGLEW" && make 51_SDL_and_modern_opengl && ./51_SDL_and_modern_opengl
